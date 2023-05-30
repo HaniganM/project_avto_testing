07c3a7254f7e548bf6e0dcbe8965d6b81b2949ae
konstantin.af <ovsanka88@mail.ru>
2023-05-30 08:04:46 +0300

    check an element on a page(assert)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,22 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
 #  Первым делом добавим конструктор — метод, который вызывается, когда мы создаем объект.
 # Конструктор объявляется ключевым словом __init__. В него в качестве параметров мы передаем экземпляр драйвера и url адрес.
 # Внутри конструктора сохраняем эти данные как аттрибуты нашего класса.
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)# добовляем неявное ожидание 10 сек
 #  Теперь добавим еще один метод open. Он должен открывать нужную страницу в браузере, используя метод get().
     def open(self):
         self.browser.get(self.url)
+
+# реализуем метод is_element_present, в котором будем перехватывать исключение.
+#В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать (строку-селектор).
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
--- test_main_page.py
+++ test_main_page.py
@@ -4,3 +4,4 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)# инициализируем Page Object, передаем в конструктор экземпляр драйвера и УРЛ адресса
     page.open() # открываем страницу
     page.go_to_login_page() # переходим на страницу логина
+    page.should_be_login_link()
